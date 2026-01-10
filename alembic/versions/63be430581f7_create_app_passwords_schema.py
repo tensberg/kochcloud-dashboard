0a7d754b3bd16b61693059eda22d18bc9e6d9075
Michael Koch <tensberg@gmx.net>
2026-01-10 17:10:38 +0100

    Passwörter erzeugen und löschen

--- alembic/versions/63be430581f7_create_app_passwords_schema.py
+++ alembic/versions/63be430581f7_create_app_passwords_schema.py
@@ -9,6 +9,7 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.sql import func
 
 
 # revision identifiers, used by Alembic.
@@ -20,24 +21,25 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
+    op.execute('CREATE EXTENSION IF NOT EXISTS "pgcrypto";')
     op.create_table(
         'user',
         sa.Column('id', sa.Integer(), primary_key=True, autoincrement=True),
         sa.Column('sub', sa.String(), nullable=False, unique=True),
         sa.Column('email', sa.String(), nullable=False, unique=True),
-        sa.Column('created', sa.DateTime(timezone=True), nullable=False),
+        sa.Column('created', sa.DateTime(timezone=True), nullable=False, server_default=func.now()),
         sa.Column('last_login', sa.DateTime(timezone=True), nullable=False),
     )
     op.create_table(
         'token',
         sa.Column('id', sa.Integer(), primary_key=True, autoincrement=True),
-        sa.Column('user_id', sa.Integer()),
+        sa.Column('user_id', sa.Integer(), nullable=False),
         sa.Column('app', sa.String(), nullable=False),
         sa.Column('description', sa.String(), nullable=True),
         sa.Column('hash', sa.String(), nullable=False),
-        sa.Column('expires', sa.DateTime(timezone=True), nullable=True),
-        sa.Column('created', sa.DateTime(timezone=True), nullable=False),
-        sa.ForeignKeyConstraint(['user_id'], ['user.id'], ),
+        sa.Column('created', sa.DateTime(timezone=True), nullable=False, server_default=func.now()),
+        sa.Column('last_used', sa.DateTime(timezone=True), nullable=True),
+        sa.ForeignKeyConstraint(['user_id'], ['user.id']),
     )
 
 
@@ -45,3 +47,4 @@ def downgrade() -> None:
     """Downgrade schema."""
     op.drop_table('token')
     op.drop_table('user')
+    op.execute('DROP EXTENSION IF EXISTS "pgcrypto";')
--- app-passwords/app-passwords.py
+++ app-passwords/app-passwords.py
@@ -1,36 +1,136 @@
 import streamlit as st
+from sqlalchemy.sql import text
+import string
+import secrets
+
+# adapt numpy dataframe to postgresql, https://stackoverflow.com/a/56766135/1095318
+import numpy as np
+from psycopg2.extensions import register_adapter, AsIs
+register_adapter(np.int64, AsIs)
 
 EMAIL_APP="dovecot"
+EMAIL_HASH_ALGO="bf"
+
+DATETIME_FORMAT="DD.MM.YYYY HH:mm"
+
+conn = st.connection("postgresql", type="sql")
 
 @st.dialog("Neues Passwort", dismissible=True)
 def create_password_form():
     with st.form("create_password"):
         description_val = st.text_input("Beschreibung", placeholder="Thunderbird Android", max_chars=40)
-        limit_validity_val = st.checkbox("Gültigkeit begrenzen")
-        validity_val = st.date_input("Gültig bis", min_value="today", format="DD.MM.YYYY")
 
         submitted = st.form_submit_button("Passwort erzeugen")
         if submitted:
-            # TODO: passwort anlegen
+            if not description_val:
+                st.error("Bitte Beschreibung angeben")
+            else:
+                st.session_state["generated_password"] = create_password(description_val)
+                st.rerun()
+
+def create_password(description):
+    password = generate_password()
+    with conn.session as session:
+        session.execute(text("INSERT INTO \"token\" (user_id,app,description,hash) VALUES (:user_id,:app,:description,CRYPT(:password, gen_salt(:hash_algo)))"), {
+            "user_id": user_id,
+            "app": EMAIL_APP,
+            "description": description,
+            "password": password,
+            "hash_algo": EMAIL_HASH_ALGO
+        })
+        session.commit()
+    return password
+
+def generate_password():
+    alphabet = string.ascii_letters + string.digits
+    return ''.join(secrets.choice(alphabet) for i in range(16))
+
+def upsert_loggedin_user():
+    with conn.session as session:
+        result = session.execute(text("""
+                        INSERT INTO "user" (sub,email,last_login) VALUES (:sub,:email, NOW())
+                        ON CONFLICT (sub) DO UPDATE SET email=:email, last_login=NOW()
+                        RETURNING id;
+                        """), {
+                            "sub": st.user.sub,
+                            "email": st.user.email
+                        })
+        session.commit()
+        return result.one()[0]
+
+@st.dialog("Passwort löschen", dismissible=True)
+def confirm_delete_password(pw_id, description):
+    st.write("Wollen Sie wirklich das Email-Passwort '{}' löschen?".format(description))
+
+    with st.container(horizontal=True, width="content"):
+        if st.button("Löschen"):
+            delete_password(pw_id)
             st.rerun()
+
+        if (st.button("Abbrechen")):
+            st.rerun()
+
+def delete_password(pw_id):
+    with conn.session as session:
+        result = session.execute(text("DELETE FROM \"token\" where id=:id"), { "id": pw_id })
+        session.commit()
+        print(result)
 
 # main application
 
 if not st.user.is_logged_in:
     st.login()
 
-conn = st.connection("postgresql", type="sql")
+if 'user_id' not in st.session_state:
+    user_id = upsert_loggedin_user()
+    st.session_state['user_id'] = user_id
+else:
+    user_id = st.session_state['user_id']
 
-st.title("App Passwörter für {}".format(st.user["name"]))
+st.title("Kochcloud Email für {}".format(st.user["name"]))
 
-st.header("Email-Passwörter für {}".format(st.user["email"]))
+st.header("Email-Konfiguration")
+
+generated_password = st.session_state.get("generated_password")
+
+with st.container(horizontal=True, vertical_alignment="center", width="content"):
+    st.write("Email-Adresse")
+    st.code(st.user["email"])
+with st.container(horizontal=True, vertical_alignment="center", width="content"):
+    st.write("Passwort")
+    if generated_password:
+        st.code(generated_password)
+        hide_password = st.button("Schließen")
+        if hide_password:
+            del st.session_state["generated_password"]
+            st.rerun()
+    else:
+        new_password_button = st.button("Neues Passwort")
+        if new_password_button:
+            create_password_form()
+
+st.header("Email-Passwörter")
 
 df = conn.query("""
-                SELECT t.id, t.description, t.expires, t.created 
-                    FROM token t,user u 
+                SELECT t.id, t.description, t.created, t.last_used
+                    FROM "token" t,"user" u 
                     WHERE t.user_id=u.id AND u.sub=:sub
                 ORDER BY t.created
                 """, ttl=0, params = { "sub": st.user["sub"]})
 
-if st.button("Neues Passwort"):
-    create_password_form()
+pw_table = st.dataframe(data=df, selection_mode="single-row", on_select="rerun",
+             placeholder="-", column_config={
+    0: None,
+    "id": None,
+    "description": "Beschreibung",
+    "created": st.column_config.DatetimeColumn("Erstellt", format=DATETIME_FORMAT),
+    "last_used": st.column_config.DatetimeColumn("Zuletzt verwendet", format=DATETIME_FORMAT)
+})
+
+pw_selected_row = pw_table.selection.rows[0] if pw_table.selection.rows else None
+
+delete_password_button = st.button("Passwort löschen", disabled=(pw_selected_row==None))
+if delete_password_button:
+    confirm_delete_password(df.at[pw_selected_row, "id"], df.at[pw_selected_row, "description"])
+
+
